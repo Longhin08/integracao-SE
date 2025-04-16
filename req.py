c3320985280081b2dd58bf5f3a504fe51ea65997
Longhin08 <jv.longhin@outlook.com>
2025-04-16 09:18:29 -0300

    Ajustes

--- req.py
+++ req.py
@@ -1,24 +1,29 @@
 import requests
-import logging
 import base64
 import os
-import time
 from dotenv import load_dotenv
-
+ 
 load_dotenv()
-
-#Arquivo de log
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='monitor.log', encoding='utf-8', format='%(message)s %(asctime)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.WARNING)
-
-# Diretório a ser monitorado
-diretorio_monitorado = r"C:\testes\monitor"
-
-# Função para monitorar a pasta e subpastas
-def monitorar_pasta(diretorio):
-    arquivos_processados = set()
-
-<<<<<<< HEAD
+ 
+ 
+# Leitura e codificação em Base64 do arquivo
+with open("teste.pdf", "rb") as file:
+    arquivo_bytes = file.read()
+    base64_bytes = base64.b64encode(arquivo_bytes)
+    file_name = "123"
+    base64_string = base64_bytes.decode("utf-8")
+ 
+# URL da sua API (substitua pela URL correta)
+url = os.getenv("DOMAIN")
+auth = os.getenv("TOKEN_JWT")
+ 
+ 
+# Headers da requisição (ajuste conforme necessário)
+headers = {
+    "Content-Type": "text/xml;charset=UTF-8",
+    "Authorization": auth
+}
+ 
 # Corpo da requisição em XML
 xml_body = f"""
     <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:document">
@@ -26,16 +31,16 @@ xml_body = f"""
     <soapenv:Body>
         <urn:newDocument>
             <urn:idcategory>naoIndex</urn:idcategory>
-            <urn:iddocument>teste com arquiv1q1231231weqwe23</urn:iddocument>
-            <urn:title>TÍTULO_DO_DOCUMEqweNTO</urn:title>
-            <urn:dsresume>RESUMO_DO_DOCUMENTO</urn:dsresume>
+            <urn:iddocument>{file_name}</urn:iddocument>
+            <urn:title>{file_name}</urn:title>
+            <urn:dsresume>Documento enviado automaticamente</urn:dsresume>
             <urn:iduser></urn:iduser>
-            <urn:attributes>caixa=123456;etiqueta=123123</urn:attributes>
+            <urn:attributes>caixa=123123;</urn:attributes>
             <urn:fgmodel></urn:fgmodel>
             <urn:file>
                 <urn:item>
-                    <urn:NMFILE>testeqweq11231231231we123.pdf</urn:NMFILE>
-                    <urn:BINFILE>{base64_string}12qwe3</urn:BINFILE>
+                    <urn:NMFILE>{file_name}.pdf</urn:NMFILE>
+                    <urn:BINFILE>{base64_string}</urn:BINFILE>
                     <urn:CONTAINER>?</urn:CONTAINER>
                     <urn:ERROR>?</urn:ERROR>
                 </urn:item>
@@ -44,68 +49,14 @@ xml_body = f"""
     </soapenv:Body>
     </soapenv:Envelope>
 """
-=======
-    logging.warning("Monitoramento Iniciado...")
-    while True:
-        for pasta_raiz, _, arquivos in os.walk(diretorio):
-            for arquivo in arquivos:
-                caminho_arquivo = os.path.join(pasta_raiz, arquivo)
-                if caminho_arquivo not in arquivos_processados:
-                    arquivos_processados.add(caminho_arquivo)
-                    logging.info(f"Novo arquivo encontrado: {arquivo}")
-                    processar_arquivo(caminho_arquivo)
->>>>>>> c1b7fc60eebd641497d6373f1027c990ce5b44db
-
-# Função para processar o arquivo
-def processar_arquivo(caminho_arquivo):
-    file_name = os.path.basename(caminho_arquivo)  # Obtém o nome do arquivo
-
-    with open(caminho_arquivo, "rb") as file:
-        arquivo_bytes = file.read()
-        base64_bytes = base64.b64encode(arquivo_bytes)
-        base64_string = base64_bytes.decode("utf-8")
-
-    auth = os.getenv("TOKEN_JWT")
-
-    url = os.getenv("DOMAIN")
-
-    headers = {
-        "Content-Type": "text/xml;charset=UTF-8",
-        "Authorization": auth
-    }
-
-    xml_body = f"""
-        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:document">
-        <soapenv:Header/>
-        <soapenv:Body>
-            <urn:newDocument>
-                <urn:idcategory>Dossiê do Curso</urn:idcategory>
-                <urn:iddocument>{file_name}</urn:iddocument>
-                <urn:title>{file_name}</urn:title>
-                <urn:dsresume>Documento enviado automaticamente</urn:dsresume>
-                <urn:iduser></urn:iduser>
-                <urn:fgmodel></urn:fgmodel>
-                <urn:file>
-                    <urn:item>
-                        <urn:NMFILE>{file_name}</urn:NMFILE>
-                        <urn:BINFILE>{base64_string}</urn:BINFILE>
-                        <urn:CONTAINER>?</urn:CONTAINER>
-                        <urn:ERROR>?</urn:ERROR>
-                    </urn:item>
-                </urn:file>
-            </urn:newDocument>
-        </soapenv:Body>
-        </soapenv:Envelope>
-    """
-
-    response = requests.post(url, headers=headers, data=xml_body)
-
-    if response.status_code == 200:
-        logging.info(f"Arquivo {file_name} enviado com sucesso!")
-        os.remove(caminho_arquivo)
-    else:
-        logging.ERROR(f"Erro ao enviar {file_name}. Código de status: {response.status_code} Resposta: {response.text}")
-
-# Iniciar a monitoração
-monitorar_pasta(diretorio_monitorado)
-
+ 
+ 
+# Realizando a requisição POST com o XML
+response = requests.post(url, headers=headers, data=xml_body)
+ 
+# Verificando a resposta da API
+if response.status_code == 200:
+    xml_response = response.text  # Supondo que a resposta da API está aqui
+    print("Resposta da API:", xml_response)
+else:
+    print(f"Erro ao enviar a requisição. Código de status: {response.text}")
